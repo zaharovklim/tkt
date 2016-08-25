f21c2c180c66141daee09f78c17fb13992ec6777
Juozapas <juozas@producters.com>
2016-08-25 14:46:46 +0300

    Fixes for prod deploy

--- bin/build_frontend.sh
+++ bin/build_frontend.sh
@@ -5,5 +5,5 @@
 source bin/env.sh
 
 dcdev build
-dcdev run --rm frontend npm run-script build
+dcdev run --rm frontend bash -c "npm run-script npminstall && npm run-script build"
 dcdev run --rm -e DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_PROD django ./bin/collectstatic.sh
