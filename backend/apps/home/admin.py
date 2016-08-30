ceb1604972736b52991ed557b36cc54f9ab852dd
Igor Lyakh <lyakhigor@gmail.com>
2016-08-30 11:18:56 +0300

    #3 Added stacked inline widget for editing Tickets from Widget admin page

--- backend/apps/home/admin.py
+++ backend/apps/home/admin.py
@@ -2,6 +2,8 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from django.contrib import admin
 
+from apps.tickets.models import Ticket
+
 from .models import Widget
 
 
@@ -16,4 +18,15 @@ UserAdmin.list_display = (
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 
-admin.site.register(Widget)
+
+class TicketInline(admin.StackedInline):
+    model = Ticket
+    extra = 1
+
+
+class WidgetAdmin(admin.ModelAdmin):
+    inlines = [
+        TicketInline,
+    ]
+
+admin.site.register(Widget, WidgetAdmin)
