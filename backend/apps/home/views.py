93f30cbd007c41b8b47d30a2e2fb533ed0061f0e
Igor Lyakh <lyakhigor@gmail.com>
2016-08-31 17:59:08 +0300

    #13 Added base statistics view

--- backend/apps/home/views.py
+++ backend/apps/home/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'home/index.html')
+
+
+def bid_statistics(request):
+    context = {
+        'statistics_data':
+            [
+                {"label": "accepted", "value": 540},
+                {"label": "paid", "value": 120},
+                {"label": "rejected", "value": 200}
+            ]
+    }
+    return render(request, 'home/statistics.html', context)
