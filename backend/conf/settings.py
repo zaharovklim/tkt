137fdc04d2bc55e213c6914e67615d196d2e667d
Kliment Zaharov <k.zaharov@simbirsoft.com>
2016-08-31 16:32:35 +0400

    6# - conflicts resolving commit

--- backend/conf/settings.py
+++ backend/conf/settings.py
@@ -12,6 +12,14 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+from easy_thumbnails.conf import Settings as thumbnail_settings
+
+
+THUMBNAIL_PROCESSORS = (
+    'image_cropping.thumbnail_processors.crop_corners',
+) + thumbnail_settings.THUMBNAIL_PROCESSORS
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -44,6 +52,8 @@ INSTALLED_APPS = [
     'ckeditor',
     'rest_framework',
     'import_export',
+    'image_cropping',
+    'easy_thumbnails',
 
     'apps.home',
     'apps.tickets',
