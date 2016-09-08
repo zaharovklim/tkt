4fea2293a500e22829315aaaf3b8ee9b530a5006
Igor Lyakh <lyakhigor@gmail.com>
2016-09-08 11:07:05 +0300

    #29 Fixed wrong import

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
-from django.conf.urls import url, include, static
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -9,5 +10,6 @@ urlpatterns = [
     url(r'^api/v1/', include('apps.api.urls')),
     url(r'^', include('apps.home.urls')),
 ]
+
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
