3a7cdec21ab25984665b4821c48d26c07d87d971
Igor Lyakh <lyakhigor@gmail.com>
2016-09-08 11:10:31 +0300

    #29 Reverted changes

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -11,5 +10,6 @@ urlpatterns = [
     url(r'^', include('apps.home.urls')),
 ]
 
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
